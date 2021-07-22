b014c17cde7f7f695b2df85a23649dc47747bd18
tony <antolisah@gmail.com>
2021-07-22 18:44:14 +0300

    query methods tests

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -12,3 +12,28 @@ it('should render same text passed as heading', () => {
   const headingElement = screen.getByRole("heading");
   expect(headingElement).toBeInTheDocument();
 })
+
+// Find By
+
+it('should render same text passed into title - findBy', async () => {
+  render(<Header title="My Header" />);
+  const headingElement = await screen.findByText(/My Header/i);
+  expect(headingElement).toBeInTheDocument();
+})
+
+// Query By
+
+it('should render same text passed into title - queryBy', async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.queryByText(/cow/i);
+  expect(headingElement).not.toBeInTheDocument();
+})
+
+// getAll By
+
+it('should render same text passed into title - getAllBy', async () => {
+  render(<Header title="My Header" />);
+  const headingElements = screen.getAllByRole("heading");
+  //expect(headingElements).toHaveLength(1);
+  expect(headingElements.length).toBe(1);
+})
